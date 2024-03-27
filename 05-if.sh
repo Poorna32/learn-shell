fab6b633afb3e956c0a6114c9f8d1fb98fa4202e
Poorna32 <poornachandraga44@gmail.com>
2024-03-27 15:53:30 +0530

    if condition

---
+++ 05-if.sh
@@ -0,0 +1,59 @@
+ simple if
+
+if [ expression ]; then
+commands
+fi
+
+if else
+if [ expression ]; then
+  commands
+else
+  commands
+fi
+
+else if [ expression1 ]; then
+  commands
+  elif[expression2]; then
+    commands
+    else
+    commands
+fi
+
+#expressions
+# 1 number expression
+<<EOF
+-eq
+-any
+-gt
+-gl
+-lt
+-le
+[ 1 -eq 1 ]
+a=10
+[ $a eq 5 ]
+EOF
+
+#2 string expression
+<<EOF
+=
+==
+-z empty
+-n not empty
+[ abc ==" $xyz" ]
+[ -z "$var" ]
+EOF
+
+#3 File expression
+
+-f
+-e
+[ -e/etc/file ]
+
+
+
+X=10
+if [ $x -gt 100 ];then
+  echo "$x is greater than 100"
+  else
+    echo "$x is less than 100 "
+fi
