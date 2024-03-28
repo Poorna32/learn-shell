ce8997e8684b6b259f0fd4237fe732cb8a1be030
Poorna32 <poornachandraga44@gmail.com>
2024-03-28 21:24:26 +0530

    colors

--- 01.printing.sh
+++ 01.printing.sh
@@ -15,8 +15,8 @@ echo hello world.
  #        \e [0m -Disable color
 
 echo -e "\e[31mHello in red color\e[0m"
-echo -e "\e[31mHello in green color\e[0m"
-echo -e "\e[31mHello in Yellow color\e[0m"
-echo -e "\e[31mHello in blue color\e[0m"
-echo -e "\e[31mHello in magenta color\e[0m"
-echo -e "\e[31mHello in cyan color\e[0m"
+echo -e "\e[32mHello in green color\e[0m"
+echo -e "\e[33mHello in Yellow color\e[0m"
+echo -e "\e[34mHello in blue color\e[0m"
+echo -e "\e[35mHello in magenta color\e[0m"
+echo -e "\e[36mHello in cyan color\e[0m"
