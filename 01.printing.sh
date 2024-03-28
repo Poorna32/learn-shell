69346a672124382a36f6d114de1ac9527271e5a8
Poorna32 <poornachandraga44@gmail.com>
2024-03-28 21:19:55 +0530

    colors

--- 01.printing.sh
+++ 01.printing.sh
@@ -1,1 +1,22 @@
 echo hello world.
+
+#color printing
+#red      -31
+#green    -32
+#yellow   -33
+#blue     -34
+#magenta  -35
+#cyan     -36
+
+#syntax : echo -e "\e[COLmMESSAGE\0m
+    #     -e  -enable color
+   #      \e[COLm - start color
+  #       MESSAGE - Message to be printed with color
+ #        \e [0m -Disable color
+
+echo -e "\e[31mHello in red color\e[0m"
+echo -e "\e[31mHello in green color\e[0m"
+echo -e "\e[31mHello in Yellow color\e[0m"
+echo -e "\e[31mHello in blue color\e[0m"
+echo -e "\e[31mHello in magenta color\e[0m"
+echo -e "\e[31mHello in cyan color\e[0m"
