72b1096dc0418e75d8870ac605dde68073b88e6d
Poorna32 <poornachandraga44@gmail.com>
2024-03-28 15:50:57 +0530

    if condition.

--- 05-if.sh
+++ 05-if.sh
@@ -1,74 +1,74 @@
-##simple if
-#
-#if [ expression ]; then
-#  commands
-#fi
-#
-#if else
-#if [ expression ]; then
-#  commands
-#else
-#  commands
-#fi
-#
-#else  if [ expression1 ]; then
-#  commands
-#  elif[expression2]; then
-#    commands
-#  else
-#    commands
-#fi
-#
-##expressions
-## 1 number expression
-#<<EOF
-#-eq
-#-any
-#-gt
-#-gl
-#-lt
-#-le
-#[ 1 -eq 1 ]
-#a=10
-#[ $a eq 5 ]
-#EOF
-#
-##2 string expression
-#<<EOF
-#=
-#==
-#-z empty
-#-n not empty
-#[ abc ==" $xyz" ]
-#[ -z "$var" ]
-#EOF
-#
-##3 File expression
-#
-#-f
-#-e
-#[ -e/etc/file ]
-#
-#
+###simple if
+##
+##if [ expression ]; then
+##  commands
+##fi
+##
+##if else
+##if [ expression ]; then
+##  commands
+##else
+##  commands
+##fi
+##
+##else  if [ expression1 ]; then
+##  commands
+##  elif[expression2]; then
+##    commands
+##  else
+##    commands
+##fi
+##
+###expressions
+### 1 number expression
+##<<EOF
+##-eq
+##-any
+##-gt
+##-gl
+##-lt
+##-le
+##[ 1 -eq 1 ]
+##a=10
+##[ $a eq 5 ]
+##EOF
+##
+###2 string expression
+##<<EOF
+##=
+##==
+##-z empty
+##-n not empty
+##[ abc ==" $xyz" ]
+##[ -z "$var" ]
+##EOF
+##
+###3 File expression
+##
+##-f
+##-e
+##[ -e/etc/file ]
+##
+##
+###
+##x=$1
+##
+##if [ $x -gt 100 ];then
+##  echo "$x is greater than 100"
+##  else
+##    echo "$x is less than 100 "
+##fi
+##
+###handle input missing
 ##
 #x=$1
-#
-#if [ $x -gt 100 ];then
+#if [ -z "$x" ]; then
+#  echo "input is missing"
+#  exit
+#fi
+##
+if [ $x -gt 100 ];then
 #  echo "$x is greater than 100"
 #  else
 #    echo "$x is less than 100 "
-#fi
-#
-##handle input missing
-#
-x=$1
-if [ -z "$x" ]; then
-  echo "input is missing"
-  exit
 fi
-#
-#if [ $x -gt 100 ];then
-#  echo "$x is greater than 100"
-#  else
-#    echo "$x is less than 100 "
-#fi
