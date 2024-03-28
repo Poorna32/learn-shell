60a0de0c8c94fc3565b01ae88a229472178ea30b
Poorna32 <poornachandraga44@gmail.com>
2024-03-28 15:54:11 +0530

    if condition.

--- 05-if.sh
+++ 05-if.sh
@@ -53,22 +53,22 @@
 ###
 ##x=$1
 ##
-##if [ $x -gt 100 ];then
-##  echo "$x is greater than 100"
-##  else
-##    echo "$x is less than 100 "
-##fi
-##
+#if [ $x -gt 100 ];then
+#  echo "$x is greater than 100"
+#  else
+#    echo "$x is less than 100 "
+#fi
+#
 ###handle input missing
 ##
 #x=$1
 #if [ -z "$x" ]; then
 #  echo "input is missing"
-#  exit
+#  exit 1
 #fi
 ##
-if [ $x -gt 100 ];then
-#  echo "$x is greater than 100"
-#  else
-#    echo "$x is less than 100 "
-fi
+#if [ $x -gt 100 ];then
+##  echo "$x is greater than 100"
+##  else
+##    echo "$x is less than 100 "
+#fi
