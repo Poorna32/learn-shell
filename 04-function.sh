ac733e1e0837bd38ac6711d0c772dd9153067bf3
Poorna32 <poornachandraga44@gmail.com>
2025-09-30 20:19:01 +0200

    Learning shell Functions

--- 04-function.sh
+++ 04-function.sh
@@ -6,11 +6,11 @@ example()
 
 
 #functions have its own variables
-example1(){
+Example1(){
   echo value of 1 - $1
   echo "value of * -$*"
   echo "value of # - $#"
 }
 
-example1() 10 20 30
-example1() "$*"
+Example1() 10 20 30
+Example1() "$*"
